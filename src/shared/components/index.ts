e450d1b8991687e28f686b15ab8e03b3ff4c7da4
Renate Gouveia <renate@horangi.com>
2018-10-22 04:08:50 +0800

    Code split via routes

--- src/shared/components/index.ts
+++ src/shared/components/index.ts
@@ -1,3 +1,5 @@
 export {default as NavBar} from "./navbar";
 export {default as LoadingView} from "./loading-view";
 export {default as Loader} from "./loader";
+export {default as AsyncComponent} from "./async-component";
+export {default as NotFoundComponent} from "./not-found";
