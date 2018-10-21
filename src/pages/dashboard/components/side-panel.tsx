62cc2e6b855ed7802cf0e3fd4b4058c94b1abf41
Renate Gouveia <renate@horangi.com>
2018-10-22 03:49:12 +0800

    Cleaned the side panel box UI

--- src/pages/dashboard/components/side-panel.tsx
+++ src/pages/dashboard/components/side-panel.tsx
@@ -1,5 +1,10 @@
 import React, { Fragment } from "react";
-import { UncontrolledTooltip } from "reactstrap";
+import {
+  UncontrolledTooltip,
+  Input,
+  InputGroup,
+  InputGroupAddon
+} from "reactstrap";
 
 interface IProps {
   txData: any;
@@ -7,42 +12,135 @@ interface IProps {
 
 const TxDetails: React.SFC<IProps> = ({ txData }) => {
   return (
-    <div style={{height: 300, width: 330, padding: 10}}>{_handleContent()}</div>
+    <div style={{ height: 390, width: 330, padding: 10 }}>
+      {_handleContent()}
+    </div>
   );
 
   function _handleContent() {
     if (txData) {
-      let shortTx = txData.txHash.substring(0, 20);
       return (
         <Fragment>
           <h4 className="text-primary">Transaction Info</h4>
           <div style={{ padding: 10 }}>
-            <h6>Type: {txData.type}</h6>
-            <h6>Status: {txData.status}</h6>
-            <h6>Date: {txData.date}</h6>
-            <h6>Block Confirmations: {txData.confirmations}</h6>
-            <h6>Source / Destination Wallet:</h6>
-            <h6>Ether Amount: {txData.value}</h6>
-            <h6>Fiat Value: {txData.fiat}</h6>
-            <h6>
-              <span style={{ fontSize: 18 }} id="tx-hash">
-                Tx ID: {shortTx}
-                ...
-              </span>
-            </h6>
-            <UncontrolledTooltip
-              style={{ width: 200 }}
-              autohide={false}
-              placement="top"
-              target="tx-hash"
-            >
-              {txData.txHash}
-            </UncontrolledTooltip>
+            <InputGroup style={{ marginBottom: 10 }}>
+              <InputGroupAddon addonType="prepend" style={{ height: 31 }}>
+                Tx ID
+              </InputGroupAddon>
+              <Input
+                placeholder="sm"
+                bsSize="sm"
+                style={{ backgroundColor: "white" }}
+                type="text"
+                value={txData.txHash}
+                disabled
+              />
+            </InputGroup>
+            <InputGroup style={{ marginBottom: 10 }}>
+              <InputGroupAddon addonType="prepend" style={{ height: 31 }}>
+                Ether
+              </InputGroupAddon>
+              <Input
+                placeholder="sm"
+                bsSize="sm"
+                style={{ backgroundColor: "white" }}
+                type="text"
+                value={txData.value}
+                disabled
+              />
+            </InputGroup>
+            <InputGroup style={{ marginBottom: 10 }}>
+              <InputGroupAddon addonType="prepend" style={{ height: 31 }}>
+                Value
+              </InputGroupAddon>
+              <Input
+                placeholder="sm"
+                bsSize="sm"
+                style={{ backgroundColor: "white" }}
+                type="text"
+                value={txData.fiat}
+                disabled
+              />
+            </InputGroup>
+            <InputGroup style={{ marginBottom: 10 }}>
+              <InputGroupAddon addonType="prepend" style={{ height: 31 }}>
+                Date
+              </InputGroupAddon>
+              <Input
+                placeholder="sm"
+                bsSize="sm"
+                style={{ backgroundColor: "white" }}
+                type="text"
+                value={txData.date}
+                disabled
+              />
+            </InputGroup>
+            <InputGroup style={{ marginBottom: 10 }}>
+              <InputGroupAddon addonType="prepend" style={{ height: 31 }}>
+                Type
+              </InputGroupAddon>
+              <Input
+                placeholder="sm"
+                bsSize="sm"
+                style={{ backgroundColor: "white" }}
+                type="text"
+                value={txData.type}
+                disabled
+              />
+            </InputGroup>
+            <InputGroup style={{ marginBottom: 10 }}>
+              <InputGroupAddon addonType="prepend" style={{ height: 31 }}>
+                Status
+              </InputGroupAddon>
+              <Input
+                invalid={txData.status !== "Complete" ? true : false}
+                valid={txData.status === "Complete" ? true : false}
+                placeholder="sm"
+                bsSize="sm"
+                style={{ backgroundColor: "white" }}
+                type="text"
+                value={txData.status}
+                disabled
+              />
+            </InputGroup>
+
+            <InputGroup style={{ marginBottom: 10 }}>
+              <InputGroupAddon addonType="prepend" style={{ height: 31 }}>
+                Blocks
+              </InputGroupAddon>
+              <Input
+                placeholder="sm"
+                bsSize="sm"
+                style={{ backgroundColor: "white" }}
+                type="text"
+                value={txData.confirmations}
+                disabled
+              />
+            </InputGroup>
+            <InputGroup style={{ marginBottom: 10 }}>
+              <InputGroupAddon addonType="prepend" style={{ height: 31 }}>
+                Source / Dest
+              </InputGroupAddon>
+              <Input
+                placeholder="sm"
+                bsSize="sm"
+                style={{ backgroundColor: "white" }}
+                type="text"
+                value={""}
+                disabled
+              />
+            </InputGroup>
           </div>
         </Fragment>
       );
     }
-    return <Fragment><h4>Select A Tx</h4><img src={require("shared/images/magnify-glass.png")} height="100" /></Fragment>;
+    return (
+      <div className="d-flex justify-content-center align-items-center flex-column" style={{height: "100%"}}>
+        <h2 className="text-primary" style={{marginBottom: 20}}>Select a Tx</h2>
+        <img src={require("shared/images/magnify-glass.png")} style={{height: 100, width: 100}} />
+        <h2 className="text-primary" style={{marginTop: 20}}>For more info...</h2>
+      </div>
+    );
   }
 };
 
