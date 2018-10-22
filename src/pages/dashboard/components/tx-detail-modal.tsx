fd7404324b002cc8bb81526548b89640092555ed
Renate Gouveia <renate@horangi.com>
2018-10-22 11:19:05 +0800

    Updated readme, added minor ui tweak to mobile modal

--- src/pages/dashboard/components/tx-detail-modal.tsx
+++ src/pages/dashboard/components/tx-detail-modal.tsx
@@ -26,126 +26,119 @@ const SelectedFileView: React.SFC<IProps> = ({ txData, toggle, isOpen }) => {
           Tx Detail
         </ModalHeader>
         <ModalBody>
-          <InputGroup style={{ marginBottom: 10 }}>
-            <InputGroupAddon addonType="prepend" style={{ height: 31 }}>
-              Tx ID
-            </InputGroupAddon>
-            <Input
-              placeholder="sm"
-              bsSize="sm"
-              style={{ backgroundColor: "white" }}
-              type="text"
-              value={txData.txHash}
-              disabled
-            />
-          </InputGroup>
-          <InputGroup style={{ marginBottom: 10 }}>
-            <InputGroupAddon addonType="prepend" style={{ height: 31 }}>
-              Ether
-            </InputGroupAddon>
-            <Input
-              placeholder="sm"
-              bsSize="sm"
-              style={{ backgroundColor: "white" }}
-              type="text"
-              value={txData.value}
-              disabled
-            />
-          </InputGroup>
-          <InputGroup style={{ marginBottom: 10 }}>
-            <InputGroupAddon addonType="prepend" style={{ height: 31 }}>
-              Value
-            </InputGroupAddon>
-            <Input
-              placeholder="sm"
-              bsSize="sm"
-              style={{ backgroundColor: "white" }}
-              type="text"
-              value={txData.txEtherFiat}
-              disabled
-            />
-          </InputGroup>
-          <InputGroup style={{ marginBottom: 10 }}>
-            <InputGroupAddon addonType="prepend" style={{ height: 31 }}>
-              Fee
-            </InputGroupAddon>
-            <Input
-              placeholder="sm"
-              bsSize="sm"
-              style={{ backgroundColor: "white" }}
-              type="text"
-              value={txData.fiat}
-              disabled
-            />
-          </InputGroup>
-          <InputGroup style={{ marginBottom: 10 }}>
-            <InputGroupAddon addonType="prepend" style={{ height: 31 }}>
-              Date
-            </InputGroupAddon>
-            <Input
-              placeholder="sm"
-              bsSize="sm"
-              style={{ backgroundColor: "white" }}
-              type="text"
-              value={txData.date}
-              disabled
-            />
-          </InputGroup>
-          <InputGroup style={{ marginBottom: 10 }}>
-            <InputGroupAddon addonType="prepend" style={{ height: 31 }}>
-              Type
-            </InputGroupAddon>
-            <Input
-              placeholder="sm"
-              bsSize="sm"
-              style={{ backgroundColor: "white" }}
-              type="text"
-              value={txData.type}
-              disabled
-            />
-          </InputGroup>
-          <InputGroup style={{ marginBottom: 10 }}>
-            <InputGroupAddon addonType="prepend" style={{ height: 31 }}>
-              Status
-            </InputGroupAddon>
-            <Input
-              invalid={txData.status !== "Complete" ? true : false}
-              valid={txData.status === "Complete" ? true : false}
-              placeholder="sm"
-              bsSize="sm"
-              style={{ backgroundColor: "white" }}
-              type="text"
-              value={txData.status}
-              disabled
-            />
-          </InputGroup>
+        <InputGroup style={{ marginBottom: 10 }}>
+              <InputGroupAddon addonType="prepend" style={{ height: 31 }}>
+                Tx ID
+              </InputGroupAddon>
+              <Input
+                bsSize="sm"
+                style={{ backgroundColor: "white" }}
+                type="text"
+                value={txData.txHash}
+                disabled
+              />
+            </InputGroup>
+            <InputGroup style={{ marginBottom: 10 }}>
+              <InputGroupAddon addonType="prepend" style={{ height: 31 }}>
+                Ether
+              </InputGroupAddon>
+              <Input
+                bsSize="sm"
+                style={{ backgroundColor: "white" }}
+                type="text"
+                value={txData.value}
+                disabled
+              />
+            </InputGroup>
+            <InputGroup style={{ marginBottom: 10 }}>
+              <InputGroupAddon addonType="prepend" style={{ height: 31 }}>
+                Value
+              </InputGroupAddon>
+              <Input
+                bsSize="sm"
+                style={{ backgroundColor: "white" }}
+                type="text"
+                value={txData.txEtherFiat}
+                disabled
+              />
+            </InputGroup>
+            <InputGroup style={{ marginBottom: 10 }}>
+              <InputGroupAddon addonType="prepend" style={{ height: 31 }}>
+                Fee
+              </InputGroupAddon>
+              <Input
+                bsSize="sm"
+                style={{ backgroundColor: "white" }}
+                type="text"
+                value={txData.fiat}
+                disabled
+              />
+            </InputGroup>
+            <InputGroup style={{ marginBottom: 10 }}>
+              <InputGroupAddon addonType="prepend" style={{ height: 31 }}>
+                Date
+              </InputGroupAddon>
+              <Input
+                bsSize="sm"
+                style={{ backgroundColor: "white" }}
+                type="text"
+                value={txData.date}
+                disabled
+              />
+            </InputGroup>
+            <InputGroup style={{ marginBottom: 10 }}>
+              <InputGroupAddon addonType="prepend" style={{ height: 31 }}>
+                Type
+              </InputGroupAddon>
+              <Input
+                bsSize="sm"
+                style={{ backgroundColor: "white" }}
+                type="text"
+                value={txData.type}
+                disabled
+              />
+            </InputGroup>
+            <InputGroup style={{ marginBottom: 10 }}>
+              <InputGroupAddon addonType="prepend" style={{ height: 31 }}>
+                Status
+              </InputGroupAddon>
+              <Input
+                invalid={txData.status !== "Complete" ? true : false}
+                valid={txData.status === "Complete" ? true : false}
+                bsSize="sm"
+                style={{ backgroundColor: "white" }}
+                type="text"
+                value={txData.status}
+                disabled
+              />
+            </InputGroup>
 
-          <InputGroup style={{ marginBottom: 10 }}>
-            <InputGroupAddon addonType="prepend" style={{ height: 31 }}>
-              Blocks
-            </InputGroupAddon>
-            <Input
-              placeholder="sm"
-              bsSize="sm"
-              style={{ backgroundColor: "white" }}
-              type="text"
-              value={txData.confirmations}
-              disabled
-            />
-          </InputGroup>
-          <InputGroup style={{ marginBottom: 10 }}>
-            <InputGroupAddon addonType="prepend" style={{ height: 31 }}>
-              Source / Dest
-            </InputGroupAddon>
-            <Input
-              placeholder="sm"
-              bsSize="sm"
-              style={{ backgroundColor: "white" }}
-              type="text"
-              value={""}
-              disabled
-            />
-          </InputGroup>
+            <InputGroup style={{ marginBottom: 10 }}>
+              <InputGroupAddon addonType="prepend" style={{ height: 31 }}>
+                Blocks
+              </InputGroupAddon>
+              <Input
+                bsSize="sm"
+                style={{ backgroundColor: "white" }}
+                type="text"
+                value={txData.confirmations}
+                disabled
+              />
+            </InputGroup>
+            <InputGroup style={{ marginBottom: 10 }}>
+              <InputGroupAddon addonType="prepend" style={{ height: 31 }}>
+                {txData.source === null ? "Destination" : "Source"}
+              </InputGroupAddon>
+              <Input
+                bsSize="sm"
+                style={{ backgroundColor: "white" }}
+                type="text"
+                value={
+                  txData.source === null ? txData.destination : txData.source
+                }
+                disabled
+              />
+            </InputGroup>
         </ModalBody>
         <ModalFooter>
           <Button color="secondary" onClick={toggle}>
